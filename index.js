dca4408613389495ff67ececc758f065ddb397b5
Roland Vachter <roland.vachter@iquestgroup.com>
2017-05-11 13:35:02 +0300

    Expose concept method.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	search: require('./lib/search'),
 	mget: require('./lib/mget'),
 	get: require('./lib/get'),
-	tag: require('./lib/tag')
+	tag: require('./lib/tag'),
+	concept: require('./lib/concept')
 };
