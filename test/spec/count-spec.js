a14defbde924863fcb6187bf1a3af169701ac151
Matt Hinchliffe <matt.hinchliffe@ft.com>
2017-06-06 16:09:53 +0100

    Standardise count spec

--- test/spec/count-spec.js
+++ test/spec/count-spec.js
@@ -1,104 +1,64 @@
-const chai = require('chai');
-const proxyquire = require('proxyquire');
-const sinon = require('sinon');
+const { expect } = require('chai');
+const nock = require('nock');
 
-chai.should();
-
-const count = require('../../lib/count');
+const subject = require('../../lib/count');
+const fixture = require('../fixtures/count.json');
 
 describe('Count', () => {
-	const signedAwsEsFetchStub = sinon.stub()
-		.resolves({
-			ok: true,
-			json: () => Promise.resolve({
-				count: 2649
-			})
-		});
-
 	afterEach(() => {
-		signedAwsEsFetchStub.resetHistory();
-	})
+		nock.isDone();
+		nock.cleanAll();
+	});
 
-	it('should exist', () => {
-		count.should.be.defined;
-	})
+	context('With options', () => {
+		it('accepts a query', () => {
+			const query = {
+				term: {
+					'metadata.idV1': 'Ng==-U2VjdGlvbnM='
+				}
+			};
 
-	it('should call the correct endpoint', () => {
-		count(undefined, undefined, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [url] = signedAwsEsFetchStub.lastCall.args;
-		url.should.equal('https://next-elastic.ft.com/content/item/_count');
-	})
+			nock('https://next-elastic.ft.com')
+				.post('/content/item/_count', (body) => {
+					return body.query.term['metadata.idV1'] === 'Ng==-U2VjdGlvbnM=';
+				})
+				.reply(200, fixture);
 
-	it('should send a POST request', () => {
-		count(undefined, undefined, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { method }] = signedAwsEsFetchStub.lastCall.args;
-		method.should.equal('POST');
-	})
-
-	it('should set a default timeout of 3000', () => {
-		count(undefined, undefined, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { timeout }] = signedAwsEsFetchStub.lastCall.args;
-		timeout.should.equal(3000);
-	})
-
-	it('should be able to set the timeout', () => {
-		count(undefined, 1000, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { timeout }] = signedAwsEsFetchStub.lastCall.args;
-		timeout.should.equal(1000);
-	})
-
-	it('should set the Content-Type to `application/json`', () => {
-		count(undefined, undefined, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { headers }] = signedAwsEsFetchStub.lastCall.args;
-		headers.should.eql({
-			'Content-Type': 'application/json'
+			return subject({ query });
 		});
-	})
+	});
 
-	it('should send a default query which matches everything', () => {
-		count(undefined, undefined, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { body }] = signedAwsEsFetchStub.lastCall.args;
-		body.should.eql(JSON.stringify({
-			query: { match_all: {} }
-		}));
-	})
-
-	it('should be able to set the query', () => {
-		const query = {
-			term: {
-				'metadata.idV1': 'Ng==-U2VjdGlvbnM='
-			}
-		};
-		count(query, undefined, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { body }] = signedAwsEsFetchStub.lastCall.args;
-		body.should.eql(JSON.stringify({ query }));
-	})
-
-	it('should be able to set the query and the timeout', () => {
-		const query = {
-			term: {
-				'metadata.idV1': 'Ng==-U2VjdGlvbnM='
-			}
-		};
-		count(query, 1000, { signedAwsEsFetch: signedAwsEsFetchStub });
-		const [, { body, timeout }] = signedAwsEsFetchStub.lastCall.args;
-		body.should.eql(JSON.stringify({ query }));
-		timeout.should.equal(1000);
-	})
-
-	it('should handle the data correctly', () => {
-		return count(undefined, undefined, { signedAwsEsFetch: signedAwsEsFetchStub })
-			.then(actualCount => actualCount.should.equal(2649));
-	})
-
-	it('should use required signedAwsEsFetch, if no dependecy given', () => {
-		const count = proxyquire('../../lib/count', {
-			'signed-aws-es-fetch': signedAwsEsFetchStub
+	context('Response - success', () => {
+		beforeEach(() => {
+			nock('https://next-elastic.ft.com')
+				.post('/content/item/_count')
+				.reply(200, fixture);
 		});
 
-		return count()
-			.then(() => {
-				signedAwsEsFetchStub.should.have.been.called;
-			});
-	})
+		it('returns a number', () => (
+			subject().then((result) => {
+				expect(result).to.be.a('number');
+				expect(result).to.equal(fixture.count);
+			})
+		));
+	});
+
+	context('Response - error', () => {
+		beforeEach(() => {
+			nock('https://next-elastic.ft.com')
+				.post('/content/item/_count')
+				.reply(500);
+		});
+
+		it('throws an HTTP error', () => (
+			subject()
+				.then((result) => {
+					expect(result).to.equal('This should never run');
+				})
+				.catch((error) => {
+					expect(error).to.be.an('error');
+					expect(error.name).to.equal('InternalServerError');
+				})
+		));
+	});
 });
