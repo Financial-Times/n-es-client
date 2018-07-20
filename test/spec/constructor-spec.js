303fccceafa87143fb1af39cdcbd36702522c4f5
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-07-20 14:48:44 +0100

    Adding test for concept and tag
     🐿 v2.10.0

--- test/spec/constructor-spec.js
+++ test/spec/constructor-spec.js
@@ -1,7 +1,7 @@
-const chai = require("chai");
-const proxyquire = require("proxyquire");
-const sinon = require("sinon");
-const sinonChai = require("sinon-chai");
+const chai = require('chai');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
 chai.should();
 chai.use(sinonChai);
@@ -10,82 +10,83 @@ const fetchStub = sinon.stub();
 let handleResponseFake;
 
 const proxy = {
-	"./helpers/handle-response": () => handleResponseFake,
-	"signed-aws-es-fetch": fetchStub,
-	"@noCallThru": true
+	'./helpers/handle-response': () => handleResponseFake,
+	'signed-aws-es-fetch': fetchStub,
+	'@noCallThru': true
 };
 
-const subject = proxyquire("../../", {
-	"./lib/search": proxyquire("../../lib/search", proxy),
-	"./lib/count": proxyquire("../../lib/count", proxy),
-	"./lib/mget": proxyquire("../../lib/mget", proxy),
-	"./lib/get": proxyquire("../../lib/get", proxy),
-	"./lib/mapping": proxyquire("../../lib/mapping", proxy)
-	// './lib/tag': proxyquire('../../lib/tag', proxy),
-	// './lib/concept': proxyquire('../../lib/concept', proxy),
+const subject = proxyquire('../../', {
+	'./lib/search': proxyquire('../../lib/search', proxy),
+	'./lib/count': proxyquire('../../lib/count', proxy),
+	'./lib/mget': proxyquire('../../lib/mget', proxy),
+	'./lib/get': proxyquire('../../lib/get', proxy),
+	'./lib/mapping': proxyquire('../../lib/mapping', proxy),
+	'./lib/concept': proxyquire('../../lib/concept', {
+		'./search': proxyquire('../../lib/search', proxy)
+	}),
 });
 
-describe("constructor with auth", () => {
+describe('constructor with auth', () => {
 	afterEach(() => {
 		fetchStub.reset();
 		handleResponseFake = null;
 	});
 
-	context("count — with constructor", () => {
-		it("passes credentials to n-es-fetch", async () => {
+	context('count — with constructor', () => {
+		it('passes credentials to n-es-fetch', async () => {
 			handleResponseFake = Promise.resolve(true);
 			fetchStub.returns(Promise.resolve(true));
 
 			await subject({
-				auth: "test-credentials"
-			}).count("test");
+				auth: 'test-credentials'
+			}).count('test');
 
 			fetchStub.should.have.been.calledWith(
 				sinon.match.defined,
 				sinon.match.defined,
-				"test-credentials"
+				'test-credentials'
 			);
 		});
 	});
 
-	context("get — with constructor", () => {
-		it("passes credentials to n-es-fetch", async () => {
+	context('get — with constructor', () => {
+		it('passes credentials to n-es-fetch', async () => {
 			fetchStub.returns(Promise.resolve(true));
 			handleResponseFake = Promise.resolve(true);
 
 			await subject({
-				auth: "test-credentials"
-			}).get("test");
+				auth: 'test-credentials'
+			}).get('test');
 
 			fetchStub.should.have.been.calledWith(
 				sinon.match.defined,
 				sinon.match.defined,
-				"test-credentials"
+				'test-credentials'
 			);
 		});
 	});
 
-	context("mget — with constructor", () => {
-		it("passes credentials to n-es-fetch", async () => {
+	context('mget — with constructor', () => {
+		it('passes credentials to n-es-fetch', async () => {
 			fetchStub.returns(Promise.resolve(true));
 			handleResponseFake = {
 				docs: []
 			};
 
 			await subject({
-				auth: "test-credentials"
-			}).mget("test");
+				auth: 'test-credentials'
+			}).mget('test');
 
 			fetchStub.should.have.been.calledWith(
 				sinon.match.defined,
 				sinon.match.defined,
-				"test-credentials"
+				'test-credentials'
 			);
 		});
 	});
 
-	context("search — with constructor", () => {
-		it("passes credentials to n-es-fetch", async () => {
+	context('search — with constructor', () => {
+		it('passes credentials to n-es-fetch', async () => {
 			fetchStub.returns(Promise.resolve(true));
 			handleResponseFake = {
 				hits: {
@@ -95,50 +96,60 @@ describe("constructor with auth", () => {
 			};
 
 			await subject({
-				auth: "test-credentials"
-			}).search("test");
+				auth: 'test-credentials'
+			}).search('test');
 
 			fetchStub.should.have.been.calledWith(
 				sinon.match.defined,
 				sinon.match.defined,
-				"test-credentials"
+				'test-credentials'
 			);
 		});
 	});
 
-	context("mapping — with constructor", () => {
-		it("passes credentials to n-es-fetch", async () => {
+	context('mapping — with constructor', () => {
+		it('passes credentials to n-es-fetch', async () => {
 			fetchStub.returns(Promise.resolve(true));
 
 			await subject({
-				auth: "test-credentials"
-			}).mapping("test");
+				auth: 'test-credentials'
+			}).mapping('test');
 
 			fetchStub.should.have.been.calledWith(
 				sinon.match.defined,
 				sinon.match.defined,
-				"test-credentials"
+				'test-credentials'
 			);
 		});
 	});
 
-	// context('concept — with constructor', () => {
-	// 	it('passes credentials to n-es-fetch', async () => {
-	// 		await subject({
-	// 			auth: 'test-credentials',
-	// 		}).concept('test');
-	//
-	// 		fetchStub.should.have.been.calledWith('test-credentials');
-	// 	});
-	// });
-	//
-	// context('tag — with constructor', () => {
-	// 	it('passes credentials to n-es-fetch', async () => {
-	// 		await subject({
-	// 			auth: 'test-credentials',
-	// 		}).tag('test');
-	//
-	// 		fetchStub.should.have.been.calledWith('test-credentials');
-	// 	});
-	// });
+	context('concept — with constructor', () => {
+		it('passes credentials to n-es-fetch', async () => {
+			fetchStub.returns(Promise.resolve(true));
+			handleResponseFake = {
+				hits: {
+					hits: [],
+					total: 0,
+				}
+			};
+
+			await subject({
+				auth: 'test-credentials',
+			}).concept('test');
+
+			fetchStub.should.have.been.calledWith(
+				sinon.match.defined,
+				sinon.match.defined,
+				'test-credentials'
+			);
+		});
+	});
+
+	context('tag — with constructor', () => {
+		it('is deprecated', async () => {
+			chai.expect(subject({
+				auth: 'test-credentials',
+			}).tag('test')).to.be.a('promise');
+		});
+	});
 });
