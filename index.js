b7d7e03db238149404c6b35620807dbaa7e02427
Simon Plenderleith <simon@simonplend.co.uk>
2020-07-01 15:20:30 +0100

    Make the Elasticsearch hostname configurable
    IMPORTANT: These changes are hacky and not intended to be merged
    or published to npm.
    
    This allows you to decide whether you want to make requests to the
    production or the development Elasticsearch cluster.
    
    Usage:
    
    1. In your Node.js app directory, install the `n-es-client`
    dependency from this branch on GitHub:
    
    ```bash
    npm install github:Financial-Times/n-es-client#use-development-cluster
    ```
    
    2. At the top of the app's `app.js` configure `n-es-client` to make
    requests to the development cluster:
    
    ```javascript
    const esClient = require('@financial-times/n-es-client');
    esClient.useDevelopmentCluster();
    ```
    
    This configuration will apply to anywhere in your app that you use
    `n-es-client`. 🐿 v2.13.0

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const logger = require('@financial-times/n-logger').default;
+
+const ELASTICSEARCH_PRODUCTION_HOSTNAME = 'next-elasticsearch.nlb.ft.com';
+const ELASTICSEARCH_DEVELOPMENT_HOSTNAME = 'search-next-elasticsearch-dev-yh4wwly56xcwvmnlfqwgliizpe.eu-west-1.es.amazonaws.com';
+
+global.ELASTICSEARCH_HOSTNAME = ELASTICSEARCH_PRODUCTION_HOSTNAME;
+
 module.exports = {
 	search: require('./lib/search'),
 	count: require('./lib/count'),
@@ -6,5 +13,13 @@ module.exports = {
 	tag: require('./lib/tag'),
 	concept: require('./lib/concept'),
 	mapping: require('./lib/mapping'),
-	msearch: require('./lib/msearch')
+	msearch: require('./lib/msearch'),
+	useDevelopmentCluster: () => {
+		global.ELASTICSEARCH_HOSTNAME = ELASTICSEARCH_DEVELOPMENT_HOSTNAME;
+
+		logger.debug({
+			event: 'ES_CLIENT_USING_DEVELOPMENT_CLUSTER',
+			ELASTICSEARCH_HOSTNAME: global.ELASTICSEARCH_HOSTNAME
+		});
+	}
 };
