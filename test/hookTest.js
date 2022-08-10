a31a7341a976717d0b54727ee981e39d39044142
Alberto Cubero Navas <alberto.cubero.parser@ft.com>
2022-08-10 13:28:41 +0200

    toolkit: added mocha

---
+++ test/hookTest.js
@@ -0,0 +1,14 @@
+const overrideEnvVars = () => {
+	const vars = {
+		NODE_ENV: 'test',
+		AWS_SIGNED_FETCH_DISABLE_DNS_RESOLUTION: true,
+	};
+
+	process.env = { ...process.env, ...vars };
+};
+
+overrideEnvVars();
+
+exports.mochaHooks = {
+	beforeAll: () => {}
+};
